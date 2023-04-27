531774c1c5faad8ef9b067022d0abb4afa923b54
Nalinaka <nalinaka@gmail.com>
2023-04-26 21:38:58 -0700

    Encapsulations exercise

--- public/ingredientList.js
+++ public/ingredientList.js
@@ -1,19 +1,20 @@
-// The array of ingredients the user can add to a sandwich
-//  This will be updated after we fetch.
-let ingredients = [];
+const ingredientList = {
 
-// Updates the DOM to display a list of ingredients
-function renderIngredientList() {
-    const ingredientUl = document.querySelector('.ingredient-list');
+    // The array of ingredients the user can add to a sandwich:
+    //  This will be updated after we fetch.
+    ingredients: [],
 
-    // Empty the ingredientList before adding any content to it.
-    ingredientUl.innerHTML = '';
-
-    ingredients.forEach(ingredient => {
-        const ingredientDiv = renderIngredientCard(ingredient);
-        ingredientUl.append(ingredientDiv)
-    })
-}
+    // Updates the DOM to display a list of ingredients:
+    render() {
+        const ingredientList = document.querySelector('.ingredient-list');
+    
+        // Empty the ingredientList before adding any content to it.
+        ingredientList.innerHTML = '';
+        ingredients.forEach(ingredient => {
+            const ingredientDiv = this.renderIngredientCard(ingredient);
+            ingredientList.append(ingredientDiv)
+        })
+    }    
 
 // Creates a DIV to display a single ingredient
 function renderIngredientCard(ingredient) {
